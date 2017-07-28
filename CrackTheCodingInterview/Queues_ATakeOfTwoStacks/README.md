317973a3e1f9257dff8504de7bfa822dfcac2bec
Robert Leathorn <rleathor@uwo.ca>
2017-07-28 12:51:02 -0400

    Add a tale of two stacks

---
+++ CrackTheCodingInterview/Queues_ATakeOfTwoStacks/README.md
@@ -0,0 +1,40 @@
+# Queues: A Tale of Two Stacks
+
+https://www.hackerrank.com/challenges/ctci-queue-using-two-stacks/problem
+
+This one was quite interesting.  I got it working by re-creating both stacks every time a push came in, but obviously this creates redundancy, and some of the test cases were failing due to timeouts.  Both stacks share numbers.
+
+My lunch was finished, so I put this aside and decided to try again the next day (today).  Last night I thought about a good way to achieve this.  Basically we use the second (or 'oldest on top') stack as a temporary holding container for incoming values.
+
+So, we put the values in that stack every time a push comes in.  They would be sorted like this.... I'll use sequential numbers to illustrate better.
+
+| Oldest        |
+| ------------- |
+| 1             |
+| 2             |
+| 3             |
+
+Now when we want to pop or top (read or remove), we will create the newest on top stack by taking the bottom values, one by one, off of the oldest on top stack.
+
+We would end up with this....
+
+| Newest        |
+| ------------- |
+| 3             |
+| 2             |
+| 1             |
+
+Let's say we pop through those, but in the meantime we have added numbers 4, 5, 6 and 7 to the oldest.  We would have this...
+
+| Oldest        |
+| ------------- |
+| 4             |
+| 5             |
+| 6             |
+| 7             |
+
+All we have to do is create the newest on top stack again, and pop or top (delete or read) accordingly.
+
+So, every time you call pop() or front() from the queue you are going to want to check if the newest on top stack has anything in it.  If it doesn't, you'll have to build it.
+
+Doing this made my solution pass for all test cases, it uses less memory, and traverses the stacks way less (we're not re-creating stacks every time we push to the queue)
---
+++ CrackTheCodingInterview/Queues_ATakeOfTwoStacks/source.cpp
@@ -0,0 +1,75 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <queue>
+using namespace std;
+
+class MyQueue {
+  
+public:
+    stack<int> stack_newest_on_top, stack_oldest_on_top;
+
+    void push(int x) {
+		//add to the storage stack - oldest will be at 'index' 0
+        stack_oldest_on_top.push(x);
+    }
+
+    void pop() {
+		//if we're out of older items, rebuild
+		//essentially we're just reversing the 
+		//oldest on top stack every time we're 
+		//out of newest on top items
+		//this lets us go through the stack
+		//as if we are traversing backwards
+        if(stack_newest_on_top.empty()) {
+            buildStack(stack_newest_on_top);
+        }
+        
+        stack_newest_on_top.pop();
+    }
+
+    int front() {
+		//see above comment in pop - we want to
+		//make sure that both situations produce
+		//expected results, so we're doing the same
+		//check for an empty newest on top stack
+		//here too
+        if(stack_newest_on_top.empty()) {
+            buildStack(stack_newest_on_top);
+        }
+        
+        return stack_newest_on_top.top();
+    }
+    
+    void buildStack(stack<int> &stack1) {
+        stack<int> newStack;
+        
+        while(!stack_oldest_on_top.empty()) {
+            stack_newest_on_top.push(stack_oldest_on_top.top());
+            stack_oldest_on_top.pop();
+        }
+    }
+};
+
+int main() {
+    MyQueue q1;
+    int q, type, x;
+    cin >> q;
+    
+    for(int i = 0; i < q; i++) {
+        cin >> type;
+        if(type == 1) {
+            cin >> x;
+            q1.push(x);
+        }
+        else if(type == 2) {
+            q1.pop();
+        }
+        else cout << q1.front() << endl;
+    }
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
